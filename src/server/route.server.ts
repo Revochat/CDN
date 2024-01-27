8d7c225605f968062b4b8f2042a0a5f4cdf15ecd
Luxchar <luccharlopeau@gmail.com>
2024-01-27 01:01:43 +0100

    setavatar debug

--- src/server/route.server.ts
+++ src/server/route.server.ts
@@ -43,7 +43,7 @@ export class RouteServer {
                     if(!user) return cb(new Error('Invalid token'), '');
             
                     (req as any).userID = user.user_id;
-            
+                    console.log(path)
                     const channelPath = path.join(this.uploadFolder, channelID);
                     const userPath = path.join(channelPath, user.user_id.toString());
             
@@ -64,7 +64,8 @@ export class RouteServer {
 
             const upload = multer({ storage: storage });
 
-            this.app.post('/upload/:channelID', upload.single('file'), async (req, res) => {
+            this.app.post('/upload/channel/:channelID', upload.single('file'), async (req, res) => {
+                Logger.info('uploading file');
                 const file = req.file;
                 if (!file) return res.status(400).json({ error: 'No file uploaded' });
                 if(!req.params.channelID) return res.status(400).json({ error: 'No channelID provided' });
@@ -74,25 +75,30 @@ export class RouteServer {
 
                 const user = await DB.connection.collection('users').findOne({ token: token });
                 if(!user) return res.status(400).json({ error: 'Invalid token' });
-                
-                const channelID = req.params.channelID
-                const channel = await DB.connection.collection('channels').findOne({ channel_id: channelID });
 
+                const channelID = req.params.channelID
+
+                // check if channel exists and if user is in it
+                const channel = await DB.connection.collection('channels').findOne({ channel_id: channelID });
                 if(!channel) return res.status(400).json({ error: 'Invalid channelID' });
 
-                if(!channel.members.includes(user.username)) return res.status(400).json({ error: 'You are not in this channel' });
+                if(!channel.members.includes(user.user_id)) return res.status(400).json({ error: 'You are not in this channel' });
 
                 // check file size
                 const stats = fs.statSync(file.path);
                 const fileSizeInBytes = stats.size;
                 const fileSizeInMegabytes = fileSizeInBytes / 1000000.0; // 1MB = 1000000 bytes
                 if(fileSizeInMegabytes > 50) return res.status(400).json({ error: 'File size is too big' });
-                
-                const fileURL = `/${req.params.channelID}/${(req as any).userID}/${file.filename}`;
+                Logger.info(`/${req.params.channelID}/${(req as any).userID}/${file.filename}`);
+
+                // const fileURL = `/${req.params.channelID}/${(req as any).userID}/${file.filename}`;
+                const fileURL = `/uploads/channels/${req.params.channelID}/${file.filename}`;
+                Logger.info(fileURL);
                 res.json({ link: fileURL });
             });
 
-            this.app.post('/upload/:userID', upload.single('file'), async (req, res) => { // for profile picture
+            this.app.post('/upload/avatar/:userID', upload.single('file'), async (req, res) => { // for profile picture
+                Logger.info('uploading profile picture');
                 const file = req.file;
                 if (!file) return res.status(400).json({ error: 'No file uploaded' });
                 if(!req.params.userID) return res.status(400).json({ error: 'No userID provided' });
@@ -100,6 +106,7 @@ export class RouteServer {
                 if(!req.token) return res.status(400).json({ error: 'No token provided' });
                 const token = req.token;
 
+                Logger.info(token);
                 const user = await DB.connection.collection('users').findOne({ token: token });
                 if(!user) return res.status(400).json({ error: 'Invalid token' });
                 
@@ -112,7 +119,12 @@ export class RouteServer {
                 const fileSizeInMegabytes = fileSizeInBytes / 1000000.0; // 1MB = 1000000 bytes
                 if(fileSizeInMegabytes > 50) return res.status(400).json({ error: 'File size is too big' });
                 
+                // set avatar in database
+                await DB.connection.collection('users').updateOne({ user_id: user.user_id }, { $set: { avatar: `/${userID}/${file.filename}` } });
+
+                Logger.info(`/${userID}/${file.filename}`);
                 const fileURL = `/profile/${userID}/${file.filename}`;
+                Logger.info(fileURL);
                 res.json({ link: fileURL });
             });
 
@@ -123,6 +135,8 @@ export class RouteServer {
             });
 
             this.app.use(express.static('uploads'));
+            this.app.use(express.static('uploads/avatars'));
+            this.app.use(express.static('uploads/channels'));
         })
     }
 
