5cca7bf495f4a249c9c007d535b0d7d3819644ae
Luxchar <luccharlopeau@gmail.com>
2024-04-03 11:25:05 +0200

    cors

--- src/server/route.server.ts
+++ src/server/route.server.ts
@@ -18,6 +18,14 @@ export class RouteServer {
 
     constructor(port: number, uploadFolder: string) {
         this.app = express();
+        // set cors headers
+
+        this.app.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+            res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+            next();
+        });
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
         this.app.use(bearerToken());
